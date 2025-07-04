d81cfc517d19ae6c9938ad99d13066847ece9207
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2025-07-04 15:07:06 +0630

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use('/static', express.static('./public'));
+
+app.set('view engine', 'pug');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res)=> {
+  const data = {
+    message: 'Hello from GeeksForGeeks!',
+    courses: ['Web Development', 'DSA', 'Java']
+  };
+  res.render('index', data);
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+app.listen(port, ()=> {
+  console.log(`Server http://localhost:${port}`);
+});
