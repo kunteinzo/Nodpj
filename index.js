2b901ff99dd17708809e0e889cd06254763120f3
Kunteinzo <theinhtetoo473@gmail.com>
2025-07-10 06:10:49 +0000

    up

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const https = require('https');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use('/static', express.static('./public'));
 
@@ -26,7 +27,19 @@ app.get('/login', (req, res) => {
   res.render('login');
 });
 
+/*
 app.listen(port, ()=> {
   console.log(`Server http://localhost:${port}`);
 });
+*/
 
+// Create HTTPS server
+const httpsServer = https.createServer({
+  key: fs.readFileSync('/etc/ssl/certs/flashapp99.run.place.key'),
+  cert: fs.readFileSync('/etc/ssl/certs/flashapp99.run.place.cer')
+}, app);
+
+// Start the server
+httpsServer.listen(443, () => {
+  console.log('HTTPS server running on port 443');
+});
